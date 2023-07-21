4d592993a17e09dcd687c57ca262dbc77976985f
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-07-21 10:32:15 +0700

    Add alias module: Acme::CPANModules::HashingPassword

---
+++ lib/Acme/CPANModules/HashingPassword.pm
@@ -0,0 +1,7 @@
+## no critic: TestingAndDebugging::RequireUseStrict
+package Acme::CPANModules::HashingPassword;
+
+use alias::module 'Acme::CPANModules::CryptingPassword';
+
+1;
+# ABSTRACT: Alias for Acme::CPANModules::CryptingPassword
